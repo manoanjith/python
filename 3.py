118daf555ce638aa32e5d19f3eb9f7351af6ea12
manoanjith <52821003+manoanjith@users.noreply.github.com>
2019-07-12 03:48:30 -0700

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,7 +1,5 @@
-ch = input("Enter a character: ")
-
-if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
- or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
-    print(ch, "is a Vowel")
+le=input('')
+if le in ('a','e','i','o','u'):
+   print("vovel")
 else:
-    print(ch, "is a Consonant")
+   print("consonant")
