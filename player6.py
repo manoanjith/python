32e696b499bc2cf62928ed68b5fca1b21bebffb4
manoanjith <52821003+manoanjith@users.noreply.github.com>
2019-07-17 16:16:05 +0530

    Update player6.py

--- player6.py
+++ player6.py
@@ -1,9 +1,8 @@
-m1,m2=map(str,input().split())
-for v in ml:
-    e=m1.count(v)
-for y in m2:
-    d=m2.count(y)
-if(e==d):
-    print("yes")
-else:
-    print("no")
+k,l=map(str,input().split())
+for i in range(len(k)):
+    if(k.count(k[i])==l.count(l[i])):
+        print("yes")
+        break
+    else:
+        print("no")
+        break
